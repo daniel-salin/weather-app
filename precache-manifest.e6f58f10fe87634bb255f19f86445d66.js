e53c0f5c70aaf4631a57a1b3776fa1d08dcb6cb6
daniel-salin <daniel.salin@chasacademy.se>
2019-03-11 16:53:55 +0100

    Updates

---
+++ precache-manifest.e6f58f10fe87634bb255f19f86445d66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c0d3e37b670e3c7a538",
+    "url": "/daniel-salin/weather-app.git/static/css/main.95137063.chunk.css"
+  },
+  {
+    "revision": "6c0d3e37b670e3c7a538",
+    "url": "/daniel-salin/weather-app.git/static/js/main.023723ff.chunk.js"
+  },
+  {
+    "revision": "749aff2072ffe846acb3",
+    "url": "/daniel-salin/weather-app.git/static/js/runtime~main.ee71e184.js"
+  },
+  {
+    "revision": "f8b62c32a9641378831f",
+    "url": "/daniel-salin/weather-app.git/static/js/2.e39fc329.chunk.js"
+  },
+  {
+    "revision": "ba9654c92edc3579b524e9c0021fcdc2",
+    "url": "/daniel-salin/weather-app.git/index.html"
+  }
+];
