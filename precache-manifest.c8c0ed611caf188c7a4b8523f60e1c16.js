e14669c4fc64c1ab22836d9a092fc78d47f82eef
daniel-salin <daniel.salin@chasacademy.se>
2019-03-12 18:04:18 +0100

    Updates

---
+++ precache-manifest.c8c0ed611caf188c7a4b8523f60e1c16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c364e2b5b273dc885c7d",
+    "url": "https://daniel-salin.github.io/weather-app/static/css/main.95137063.chunk.css"
+  },
+  {
+    "revision": "c364e2b5b273dc885c7d",
+    "url": "https://daniel-salin.github.io/weather-app/static/js/main.8e3d1fcd.chunk.js"
+  },
+  {
+    "revision": "a03c8c27f8322e1524d1",
+    "url": "https://daniel-salin.github.io/weather-app/static/js/runtime~main.bcaf50e0.js"
+  },
+  {
+    "revision": "bc6dd81a996f23ed7e43",
+    "url": "https://daniel-salin.github.io/weather-app/static/js/2.fb993bc9.chunk.js"
+  },
+  {
+    "revision": "21b38e73aa1bf61255ee38997cd4c85b",
+    "url": "https://daniel-salin.github.io/weather-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/daniel-salin/weather-app.git/precache-manifest.81259a8862ca2a86eb7d946c90523b59.js"
+  "https://daniel-salin.github.io/weather-app/precache-manifest.c8c0ed611caf188c7a4b8523f60e1c16.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/daniel-salin/weather-app.git/index.html", {
+workbox.routing.registerNavigationRoute("https://daniel-salin.github.io/weather-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
